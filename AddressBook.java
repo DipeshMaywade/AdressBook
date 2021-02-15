4ccd3c86564f2276d7745fa892371e1581e52555
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-15 14:30:14 +0530

    [Deep]Updated: update for ability to create new contact in adressBook

---
+++ AddressBook.java
@@ -0,0 +1,36 @@
+
+public class AddressBook {
+        private final String firstName;
+        private final String lastName;
+        private final String address;
+        private final String city;
+        private final String state;
+        private final String zip;
+        private final String phone;
+        private final String email;
+
+
+        public AddressBook(String firstName, String lastName, String address, String city,
+                               String state, String zip, String phone, String email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+        public String toString(){
+            return "First Name:"+firstName+"\nLast Name:"+lastName+"\nAddress:"+address+
+                     "\nState:"+state+"\nCity:"+city+"\nZIP:"+zip+"\nPhone:"+phone+"\nEmail:"+email;
+        }
+
+        public static void main(String[] args) {
+            AddressBook obj = new AddressBook("Dipesh", "Maywade", "Betul", "Betul",
+                    "MadhyaPradesh", "460001", "8269602271", "dipshmaywade@gmail.com");
+
+            System.out.println(obj.toString());
+        }
+}
+
--- AdressBookMain.java
+++
@@ -1,5 +0,0 @@
-public class AdressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Address Book");
-    }
-}
